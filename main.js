48686a54eaea69f6661c3b32fb2d7fbb96a31397
kareemIsWebDeveloper <kareem.fullstackdeveloper@gmial.com>
2023-02-15 21:17:25 +0200

    social-proof-section-master

---
+++ main.js
@@ -0,0 +1,11 @@
+// (function (){
+//     var star = document.querySelector("li");
+//     var myStar = document.createElement("IMG");
+//     myStar.setAttribute("src", "./assets/images/icon-star.svg");
+//     myStar.setAttribute("width", "20px");
+//     myStar.setAttribute("height", "20px");
+//     myStar.setAttribute("alt", "image");
+//     for (var i = 0; i < 6; i++) {
+//         star[i].appendChild(myStar);
+//     }
+// })();
